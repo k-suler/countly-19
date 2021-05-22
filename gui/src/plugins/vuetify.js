278e5687f58b81a2dcbe000775886ad4fb894f89
k-suler <kristijan.suler@gmail.com>
2021-05-22 15:11:09 +0200

    Change: theme

--- gui/src/plugins/vuetify.js
+++ gui/src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        primary: "#80bef5",
+        secondary: "#f79f77",
+        accent: "#f6d58a",
+        info: "#010101",
+        error: "#f44336",
+        warning: "#ff5722",
+        success: "#4caf50",
+      },
+    },
+  },
+});
