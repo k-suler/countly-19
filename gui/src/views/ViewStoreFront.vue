b21f17c480df322ffb6f3c3846309bed928b2db8
mihaarh <ma6809@student.uni-lj.si>
2021-05-22 16:36:32 +0200

    Add: component for displaying animation

--- gui/src/views/ViewStoreFront.vue
+++ gui/src/views/ViewStoreFront.vue
@@ -1,10 +1,24 @@
 <template>
-
+  <v-container fluid class="fill-height">
+    <v-row align="center" justify="start">
+      <v-col align-self="center">
+        <BlobOrganicCircle/>
+      </v-col>
+    </v-row>
+    <v-row align="center" justify="start">
+      <v-col align-self="center">
+        <BaseCounter/>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
+import BlobOrganicCircle from "@/components/storefront/BlobOrganicCircle";
+import BaseCounter from "@/components/storefront/BaseCounter";
 export default {
-  name: "ViewStoreFront"
+  name: "ViewStoreFront",
+  components: {BaseCounter, BlobOrganicCircle}
 }
 </script>
 
