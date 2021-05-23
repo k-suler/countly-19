08394d638ddd261bdb07802ee9b1e308e949ddb1
k-suler <kristijan.suler@gmail.com>
2021-05-23 03:15:52 +0200

    Add: chart.js

--- gui/src/router/index.js
+++ gui/src/router/index.js
@@ -2,57 +2,57 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 // import DashboardPage from "../components/dashboard/dashboard.vue";
-import {auth} from '@/firebase/firebase'
+import { auth } from "@/firebase/firebase";
 import Login from "../components/Login.vue";
 import Signup from "../components/Signup.vue";
 import ViewStoreFront from "@/views/ViewStoreFront";
-
+import RandomChart from "@/components/RandomChart.vue";
 Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home,
-    },
-    {
-        path: "/about",
-        name: "About",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () =>
-            import(/* webpackChunkName: "about" */ "../views/About.vue"),
-    },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
 
-    {path: "/signup", name: "signup", component: Signup},
-    {path: "/login", name: "login", component: Login},
-    // { path: "/dashboard", component: DashboardPage },
-    {
-        path: "/storefront",
-        name: "Storefront",
-        component: ViewStoreFront,
-        meta: {
-            requiresAuth: true
-        }
-
+  { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
+  { path: "/chart", name: "chart", component: RandomChart },
+  // { path: "/dashboard", component: DashboardPage },
+  {
+    path: "/storefront",
+    name: "Storefront",
+    component: ViewStoreFront,
+    meta: {
+      requiresAuth: true,
     },
+  },
 ];
 
 const router = new VueRouter({
-    mode: "history",
-    base: process.env.BASE_URL,
-    routes,
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 
 router.beforeEach((to, from, next) => {
-    const requiresAuth = to.matched.some(x => x.meta.requiresAuth)
+  const requiresAuth = to.matched.some((x) => x.meta.requiresAuth);
 
-    if (requiresAuth && !auth.currentUser) {
-        next('/login')
-    } else {
-        next()
-    }
-})
+  if (requiresAuth && !auth.currentUser) {
+    next("/login");
+  } else {
+    next();
+  }
+});
 
 export default router;
