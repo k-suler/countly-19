e51b215aa318f9e867cff69fcd55fc693d6551b4
MihaArh <MihaArh@users.noreply.github.com>
2021-05-23 12:42:04 +0200

    Update firebase.js

--- gui/src/firebase/firebase.js
+++ gui/src/firebase/firebase.js
@@ -10,17 +10,6 @@ import 'firebase/analytics'
 
 
 
-// firebase init - add your own config here
-const firebaseConfig = {
-    apiKey: "AIzaSyD3Q1oHXOFNtRB7vxu63rtQk8aI18oUSg4",
-    authDomain: "countly-19.firebaseapp.com",
-    databaseURL: "https://countly-19-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "countly-19",
-    storageBucket: "countly-19.appspot.com",
-    messagingSenderId: "809868506187",
-    appId: "1:809868506187:web:4f987deadd25749dab49ce",
-    measurementId: "G-DMS9174PCJ"
-};
 firebase.initializeApp(firebaseConfig)
 
 // export default firebase.database();
